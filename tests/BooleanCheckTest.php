b1a032b1da50b0a93b33a9a11af21ecc55aaada4
Julien VITTE <julien.vitte@insidegroup.fr>
2021-02-10 23:46:58 +0100

    tests: cleans code style

--- tests/BooleanCheckTest.php
+++ tests/BooleanCheckTest.php
@@ -2,22 +2,8 @@
 
 namespace Tests\Pitchart\Phlunit;
 
-use GuzzleHttp\Psr7\Response;
-use PHPUnit\Framework\Assert;
-use PHPUnit\Framework\Constraint\Exception as ExceptionConstraint;
-use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
-use Pitchart\Phlunit\Checks\ArrayCheck;
-use Pitchart\Phlunit\Checks\CallableCheck;
-use Pitchart\Phlunit\Checks\CollectionCheck;
-use Pitchart\Phlunit\Checks\GenericCheck;
-use Pitchart\Phlunit\Checks\DateTimeCheck;
-use Pitchart\Phlunit\Checks\ResponseCheck;
-use Pitchart\Phlunit\Checks\StringCheck;
 use Pitchart\Phlunit\Check;
-use Psr\Http\Message\ResponseInterface;
-use Tests\Pitchart\Phlunit\Fixture\Custom;
-use Tests\Pitchart\Phlunit\Fixture\CustomAssertion;
 
 class BooleanCheckTest extends TestCase
 {
