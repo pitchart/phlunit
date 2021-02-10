ed3e0da20758488ac7ee90d6113a41ec3148bf8f
Julien VITTE <julien.vitte@insidegroup.fr>
2021-02-10 23:21:55 +0100

    feat(datetime): adds comparison checks

--- src/Checks/DateTimeCheck.php
+++ src/Checks/DateTimeCheck.php
@@ -29,7 +29,56 @@ class DateTimeCheck implements FluentCheck
         $this->value = $date;
     }
 
+    public function hasYear(int $year): self
+    {
+        Assert::assertSame($year, (int) $this->value->format('Y'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function hasMonth(int $month): self
+    {
+        Assert::assertSame($month, (int) $this->value->format('m'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function hasDay(int $day): self
+    {
+        Assert::assertSame($day, (int) $this->value->format('d'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function hasHours(int $hours): self
+    {
+        Assert::assertSame($hours, (int) $this->value->format('H'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function hasMinutes(int $minutes): self
+    {
+        Assert::assertSame($minutes, (int) $this->value->format('i'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function hasSeconds(int $seconds): self
+    {
+        Assert::assertSame($seconds, (int) $this->value->format('s'), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
     public function isSameDayAs(\DateTimeInterface $expected): self
+    {
+        Assert::assertThat($this->value, new IsSameDayAs($expected), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function isSameTimeAs(\DateTimeInterface $expected): self
     {
         Assert::assertThat($this->value, new IsSameDayAs($expected), $this->message);
         $this->resetMessage();
@@ -39,6 +88,28 @@ class DateTimeCheck implements FluentCheck
     public function isSameIgnoringMillis(\DateTimeInterface $expected): self
     {
         Assert::assertThat($this->value, new IsSameIgnoringMillis($expected), $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function isAfter(\DateTimeInterface $date)
+    {
+        Assert::assertGreaterThan($date, $this->value, $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function isBefore(\DateTimeInterface $date)
+    {
+        Assert::assertLessThan($date, $this->value, $this->message);
+        $this->resetMessage();
+        return $this;
+    }
+
+    public function isBetween(\DateTimeInterface $from, \DateTimeInterface $to)
+    {
+        Assert::assertGreaterThanOrEqual($from, $this->value, $this->message);
+        Assert::assertLessThanOrEqual($to, $this->value, $this->message);
         $this->resetMessage();
         return $this;
     }
--- tests/DateTimeCheckTest.php
+++ tests/DateTimeCheckTest.php
@@ -17,7 +17,7 @@ class DateTimeCheckTest extends TestCase
         try {
             Check::that(new \DateTime())
                 ->withMessage('toto')
-                ->isSameDayAs(\DateTime::createFromFormat('Y-m-d','1983-28-04'))
+                ->isSameDayAs(\DateTime::createFromFormat('Y-m-d','1983-04-28'))
             ;
         } catch (ExpectationFailedException $exception) {
             Assert::assertRegExp('/toto/', $exception->getMessage());
@@ -32,11 +32,46 @@ class DateTimeCheckTest extends TestCase
         ;
     }
 
+    public function test_checks_datetime_parts()
+    {
+        $date = \DateTime::createFromFormat('Y-m-d H:i:s','1983-04-28 00:30:45');
+
+        Check::that($date)
+            ->hasYear(1983)
+            ->hasMonth(4)
+            ->hasDay(28)
+            ->hasHours(0)
+            ->hasMinutes(30)
+            ->hasSeconds(45)
+        ;
+    }
+
+    public function test_checks_date_is_before_expected()
+    {
+        Check::that(\DateTime::createFromFormat('Y-m-d','1983-04-28'))->isBefore(new \DateTime());
+    }
+
+    public function test_checks_date_is_after_expected()
+    {
+        Check::that(new \DateTime())->isAfter(\DateTime::createFromFormat('Y-m-d','1983-04-28'));
+    }
+
+    public function test_checks_date_is_between_two_dates()
+    {
+        Check::that(new \DateTime())
+            ->withMessage('May this message be seen one day ...')
+            ->isBetween(
+                \DateTime::createFromFormat('Y-m-d','1983-04-28'),
+                \DateTime::createFromFormat('Y-m-d','2121-02-10')
+            )
+        ;
+    }
+
     public function test_fails_for_bad_datetime_comparisons()
     {
         try {
             Check::that(new \DateTime())
-                ->isSameDayAs(\DateTime::createFromFormat('Y-m-d','1983-28-04'))
+                ->isSameDayAs(\DateTime::createFromFormat('Y-m-d','1983-04-28'))
             ;
         } catch (ExpectationFailedException $exception) {
             Assert::assertThat($exception, new ExceptionConstraint(ExpectationFailedException::class));
